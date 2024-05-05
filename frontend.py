f2c87bd5cd28c74f3121e5c2ae43e4e44b500f6d
Frenz86 <frenzy86@gmail.com>
2024-05-05 12:56:49 +0200

    ww

--- frontend.py
+++ frontend.py
@@ -6,8 +6,13 @@ async def test(num1, num2):
     async with aiohttp.ClientSession() as session:
         async with session.get(f'http://localhost:8000/sum?num1={num1}&num2={num2}') as response:
             result = await response.json()
-            res = result['result']
-            return res
+            return result['result']
+
+
+async def compute_and_update_result(num1, num2):
+    somma = await test(num1, num2)
+    #ui.markdown(f'risultato : {somma}')
+    ui.notify(f'risultato : {somma}')
 
 
 def init(fastapi_app: FastAPI) -> None:
@@ -23,8 +28,7 @@ def init(fastapi_app: FastAPI) -> None:
         num1 = ui.number(label='Primo numero', value=5.6, format='%.2f',).bind_value(app.storage.user, 'num1')
         num2 = ui.number(label='Secondo numero', value=15.2, format='%.2f',).bind_value(app.storage.user, 'num2')
         #button = ui.button('risultato', on_click=lambda: ui.notify("IL RISULTATO DELLA SOMMA E'"+str(num1.value+num2.value)))  ## this works!
-        result = await test(num1.value, num2.value)
-        button = ui.button('risultato', on_click=lambda: ui.notify("IL RISULTATO DELLA SOMMA E'"+str(result)))                  ## this don't work when I change input!
+        button = ui.button('risultato', on_click=lambda: compute_and_update_result(num1.value, num2.value))
 
 
     @ui.page('/pag2')
